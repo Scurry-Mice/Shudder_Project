b538183ca76d709f08be2e7bfe77c29ba405d4d3
lesanger <basiys@yandex.ru>
2020-04-21 01:02:26 +0300

    Inventory Added

---
+++ Assets/Scripts/PickUp.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject itemButton;
+
+    void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Hero_Kitty").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Hero_Kitty"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    // ITEAM CAN BE ADDED TO INVENTORY! 
+                    inventory.isFull[i] = true;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+    }
+}
