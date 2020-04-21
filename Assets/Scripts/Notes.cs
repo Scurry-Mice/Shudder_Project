ca6f358ee39fdb64a7a4616d60b52c252e1fde7f
Dmitriy Malevantsev <maaleex.den@hotmail.com>
2020-04-21 04:29:20 +0300

    v 0.25.335f

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    internal static string Notes_1 = "Странная записка... \n\nПо ночам часто слышен чей-то топот, за ним всегда следует визг животных, что бы это могло значить?";
+}
