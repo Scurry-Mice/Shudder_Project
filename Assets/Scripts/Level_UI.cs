796de7d438aa3fb0196830d1f4b7e3593886b9ef
lesanger <basiys@yandex.ru>
2020-04-23 02:52:28 +0300

    v 0.40.100 Changed UI

--- Assets/Scripts/Level_UI.cs
+++ Assets/Scripts/Level_UI.cs
@@ -7,47 +7,31 @@ public class Level_UI : MonoBehaviour
 {
     // PAUSE
     [SerializeField] private GameObject Pause_Panel;
-    [SerializeField] private GameObject Pause_Button_Panel_GO;
-    [SerializeField] private GameObject Back_Menu_Panel_GO;
-    [SerializeField] private GameObject Exit_Game_Panel_GO;
-    [SerializeField] private GameObject Panel_Setting_LVL;
     internal static GameObject Panel_RESTART;
 
+    //Notes
     [SerializeField] internal static GameObject Panel_Zapisok;
     [SerializeField] internal static bool Bool_Notepad = false;
 
     // UI
     [SerializeField] private GameObject UI_Panel;
-    [SerializeField] private GameObject Help_Panel;
 
     // Start is called before the first frame update
     void Start()
     {
         //получаем GO на старте.
-        Pause_Panel = GameObject.Find("Canvas/Panel_Pause");
-        Pause_Button_Panel_GO = GameObject.Find("Canvas/Panel_Pause/Pause_Button_Panel");
-        Back_Menu_Panel_GO = GameObject.Find("Canvas/Panel_Pause/Back_Menu_Panel");
-        Exit_Game_Panel_GO = GameObject.Find("Canvas/Panel_Pause/Exit_Game_Panel");
-        Panel_Setting_LVL = GameObject.Find("Canvas/Panel_Pause/Panel_Setting");
+        Pause_Panel = GameObject.Find("Canvas/Pause_Panel");
 
         Panel_Zapisok = GameObject.Find("Canvas/NotePad");
         Panel_RESTART = GameObject.Find("Canvas/Panel_Restart");
 
         UI_Panel = GameObject.Find("Canvas/Panel_UI");
-        Help_Panel = GameObject.Find("Canvas/Panel_UI/Panel_Help");
-
-        Pause_Button_Panel_GO.SetActive(false);
+       
         Pause_Panel.SetActive(false);
-        Back_Menu_Panel_GO.SetActive(false);
-        Exit_Game_Panel_GO.SetActive(false);
-        Panel_Setting_LVL.SetActive(false);
-
+       
         Panel_RESTART.SetActive(false);
 
-        Help_Panel.SetActive(false);
-
-        Panel_Zapisok.SetActive(false);
-        
+        Panel_Zapisok.SetActive(false);       
     }
 
     
@@ -57,76 +41,21 @@ public class Level_UI : MonoBehaviour
         Panel_RESTART.SetActive(true);
     }
 
-    /*
-    private void LateUpdate()
-    {
-        if (!Panel_Zapisok)
-        {
-            Debug.Log("TEST");
-        }
-
-        if (Bool_Notepad)
-        {
-            Debug.Log("!!!!!!!!!!!!!!!!");
-        }
-        else
-        {
-            Debug.Log("________________");
-        }
-
-        //Bool_Notepad = Panel_Zapisok;
-    }
-    */
-
 
     // Update is called once per frame
     void Update()
     {
-
-        /*
-        if (Input.GetKeyDown(KeyCode.Q))
+        if (Input.GetKeyDown(KeyCode.Escape) && !Bool_Notepad)
         {
-            Panel_Zapisok.SetActive(true);
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            Panel_Zapisok.SetActive(false);
-        }
-        */
-
-        if (Input.GetKeyDown(KeyCode.Escape) && !Bool_Notepad/* && !Panel_Zapisok*/)
-        {
-            Pause_Button_Panel_GO.SetActive(true);
             Pause_Panel.SetActive(true);
             UI_Panel.SetActive(false);
-
         }
 
         if (Panel_Zapisok && Input.GetKeyDown(KeyCode.Escape))
         {
             Panel_Zapisok.SetActive(false);
             Bool_Notepad = false;
-        }
-
-        if (Pause_Button_Panel_GO && Input.GetKeyDown(KeyCode.Escape))
-        {
-            Pause_Button_Panel_GO.SetActive(true);
-            Exit_Game_Panel_GO.SetActive(false);
-        }
-
-        if (Panel_Setting_LVL&& Input.GetKeyDown(KeyCode.Escape))
-        {
-            Panel_Setting_LVL.SetActive(false);
-            Pause_Button_Panel_GO.SetActive(true);
-        }
-
-        
-    }
-
-    public void Button_Setting_panel()
-    {
-        Panel_Setting_LVL.SetActive(true);
-        Pause_Button_Panel_GO.SetActive(false);
+        }        
     }
 
     internal static string Find_Notes_TXT(string Text)
@@ -135,47 +64,21 @@ public class Level_UI : MonoBehaviour
 
         return TEXT;
     }
-    
-    public void Exit_Button_Panel()
-    {
-        Pause_Button_Panel_GO.SetActive(false);
-        Exit_Game_Panel_GO.SetActive(true);
-    }
 
-    public void Back_Button()
+    public void ContinueGame()
     {
-        Pause_Button_Panel_GO.SetActive(false);
         Pause_Panel.SetActive(false);
         UI_Panel.SetActive(true);
     }
 
-    public void Back_to_Menu_panel()
-    {
-        Pause_Button_Panel_GO.SetActive(false);
-        Back_Menu_Panel_GO.SetActive(true);
-    }
-
     //выход в меню
-    public void Back_Yes()
+    public void BackToMenu()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         if (Panel_RESTART)
         {
             Panel_RESTART.SetActive(false);
         }
-    }
-
-    public void Back_No()
-    {
-        Pause_Button_Panel_GO.SetActive(true);
-        Back_Menu_Panel_GO.SetActive(false);
-        Exit_Game_Panel_GO.SetActive(false);
-    }
-
-    // кнопка подтверждения выхода (Yes)
-    public void Exit_Button()
-    {
-        Application.Quit();
     }
 
     public void RESTART ()
--- Assets/Scripts/Menu_Script.cs
+++ Assets/Scripts/Menu_Script.cs
@@ -15,12 +15,8 @@ public class Menu_Script : MonoBehaviour
     {
         Menu_Panel = GameObject.Find("Canvas/Panel_Menu");
         Setting_Panel = GameObject.Find("Canvas/Panel_Settings");
-        Level_Panel = GameObject.Find("Canvas/Panel_Levels");
-        Exit_Panel = GameObject.Find("Canvas/Panel_Exit");
         Autors_Panel = GameObject.Find("Canvas/Panel_Autors");
 
-        Exit_Panel.SetActive(false);
-        Level_Panel.SetActive(false);
         Setting_Panel.SetActive(false);
         Autors_Panel.SetActive(false);
     }
@@ -28,23 +24,15 @@ public class Menu_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Exit_Panel && Input.GetKeyDown(KeyCode.Escape) || Setting_Panel && Input.GetKeyDown(KeyCode.Escape) || Level_Panel && Input.GetKeyDown(KeyCode.Escape) || Autors_Panel && Input.GetKeyDown(KeyCode.Escape))
+        if (Setting_Panel && Input.GetKeyDown(KeyCode.Escape) || Level_Panel && Input.GetKeyDown(KeyCode.Escape) || Autors_Panel && Input.GetKeyDown(KeyCode.Escape))
         {
             Back_To_Menu_Button();
         }
     }
 
-    public void New_Game_button(bool Control)
+    public void StartGame()
     {
-        if (Control)
-        {
-            Menu_Panel.SetActive(false);
-            Level_Panel.SetActive(true);
-        }
-        else
-        {
-            Back_To_Menu_Button();
-        }
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
     }
 
     public void Back_To_Menu_Button()
@@ -56,14 +44,7 @@ public class Menu_Script : MonoBehaviour
         {
             Setting_Panel.SetActive(false);
         }
-        if (Exit_Panel)
-        {
-            Exit_Panel.SetActive(false);
-        }
-        if (Level_Panel)
-        {
-            Level_Panel.SetActive(false);
-        }
+   
         if (Autors_Panel)
         {
             Autors_Panel.SetActive(false);
@@ -77,26 +58,14 @@ public class Menu_Script : MonoBehaviour
         Menu_Panel.SetActive(false);
     }
 
-    //загрузка нужного уровня
-    public void Load_level(int Num_Scene)
-    {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(Num_Scene);
-    }
 
-    public void Button_setting()
+    public void Settings_Button()
     {
         Menu_Panel.SetActive(false);
         Setting_Panel.SetActive(true);
     }
 
-    //Вызов панели выхода
-    public void Button_Exit_Panel()
-    {
-        Menu_Panel.SetActive(false);
-        Exit_Panel.SetActive(true);
-    }
-
-    // кнопка подтверждения выхода (Yes)
+    // кнопка выхода
     public void Exit_Button()
     {
         Application.Quit();
