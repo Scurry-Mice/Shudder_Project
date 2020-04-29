514ccb7263be666d97c35a16c56471e7037c6193
lesanger <basiys@yandex.ru>
2020-04-29 20:29:40 +0300

    v 0.40.902 Changed scene settings

--- Assets/_Scripts/UI/DownloadMenu.cs
+++ Assets/_Scripts/UI/DownloadMenu.cs
@@ -17,8 +17,8 @@ public class DownloadMenu : MonoBehaviour
 
     void SetIntro1()
     {
-        string text = "Всегда что-то происходит первый раз…";
-        string TEXT = GameObject.Find("Canvas/Text").GetComponent<Text>().text = text;
+        string text = "Всегда что-то случается в первый раз…";
+        //string TEXT = GameObject.Find("Canvas/Text").GetComponent<Text>().text = text;
 
         gameObject.GetComponent<AudioSource>().PlayOneShot(audioClipArray[0]);
     }
@@ -26,7 +26,7 @@ public class DownloadMenu : MonoBehaviour
     void SetIntro2()
     {
         string text = "Именно поэтому, никто не ожидал землетрясения в центре России!";
-        string TEXT = GameObject.Find("Canvas/Text").GetComponent<Text>().text = text;
+        //string TEXT = GameObject.Find("Canvas/Text").GetComponent<Text>().text = text;
 
         gameObject.GetComponent<AudioSource>().PlayOneShot(audioClipArray[1]);        
     }
